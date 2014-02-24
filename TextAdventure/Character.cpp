4dc238dcd6712207496145b577485da40d362eed
John Cognetti <cognettings@gmail.com>
2014-02-23 20:45:52 -0500

    Implement Character
    Basic functionality

--- TextAdventure/Character.cpp
+++ TextAdventure/Character.cpp
@@ -1,11 +1,69 @@
 #include "Character.hpp"
 
-
-Character::Character(void)
-{
+// Getters and setters
+const Character::ItemMap& Character::inventory() const {
+	return this->_inventory;
 }
 
 
-Character::~Character(void)
-{
+void Character::setInventory(const ItemMap& newVal) {
+	this->_inventory = newVal;
+}
+
+
+const Character::ItemMap& Character::equipment() const {
+	return this->_equipment;
+}
+
+
+void Character::setEquipment(const ItemMap& newVal) {
+	this->_equipment = newVal;
+}
+
+
+const Stats& Character::baseStats() const {
+	return this->_baseStats;
+}
+
+
+void Character::setBaseStats(const Stats& newVal) {
+	this->_baseStats = newVal;
+}
+
+
+const Room* Character::room() const {
+	return this->_room;
+}
+
+
+void Character::setRoom(Room* newVal) {
+	this->_room = newVal;
+}
+
+
+// Constructors, deconstructor, and operators
+Character::Character(void) {
+	this->_room = nullptr;
+}
+
+
+Character::Character(const Character& other) {
+	this->_inventory = other._inventory;
+	this->_equipment = other._equipment;
+	this->_baseStats = other._baseStats;
+	this->_room = other._room;
+}
+
+
+Character::~Character(void) {
+}
+
+
+Character& Character::operator=(const Character& rhs) {
+	this->_inventory = rhs._inventory;
+	this->_equipment = rhs._equipment;
+	this->_baseStats = rhs._baseStats;
+	this->_room = rhs._room;
+
+	return *this;
 }
--- TextAdventure/Character.hpp
+++ TextAdventure/Character.hpp
@@ -1,10 +1,40 @@
-#pragma once
+#ifndef _CHARACTER_HPP
+#define _CHARACTER_HPP
+
+#include <map>
 #include "GameObject.hpp"
+#include "Item.hpp"
+#include "Stats.hpp"
+#include "Room.hpp"
+
 class Character :
-	public GameObject
-{
-public:
+public GameObject {
+typedef std::map<const char*, Item*> ItemMap;
+
+
+private:	// Properties
+	ItemMap _inventory;
+	ItemMap _equipment;
+	Stats _baseStats;
+	Room* _room;
+
+
+public:		// Getters and setters
+	const ItemMap& inventory() const;
+	void setInventory(const ItemMap& newVal);
+	const ItemMap& equipment() const;
+	void setEquipment(const ItemMap& newVal);
+	const Stats& baseStats() const;
+	void setBaseStats(const Stats& newVal);
+	const Room* room() const;
+	void setRoom(Room* newVal);
+
+
+public:		// Constructors, deconstructor, and operators
 	Character(void);
+	Character(const Character& other);
 	virtual ~Character(void);
+	Character& operator=(const Character& rhs);
 };
 
+#endif
