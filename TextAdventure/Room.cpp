3c09ec73edca1fa196d4929e816f011f46a08ea6
John Cognetti <cognettings@gmail.com>
2014-03-03 11:36:51 -0500

    Implement Rooms

--- TextAdventure/Room.cpp
+++ TextAdventure/Room.cpp
@@ -1,11 +1,116 @@
+#include <map>
 #include "Room.hpp"
 
-
-Room::Room(void)
-{
+// Getters and setters
+Room* Room::up() const {
+	return this->_up;
 }
 
 
-Room::~Room(void)
-{
+void Room::setUp(Room* newVal) {
+	this->_up = newVal;
+	newVal->_down = this;
 }
+
+
+Room* Room::down() const {
+	return this->_down;
+}
+
+
+void Room::setDown(Room* newVal) {
+	this->_down = newVal;
+	newVal->_up = this;
+}
+
+
+Room* Room::left() const {
+	return this->_left;
+}
+
+
+void Room::setLeft(Room* newVal) {
+	this->_left = newVal;
+	newVal->_right = this;
+}
+
+
+Room* Room::right() const {
+	return this->_right;
+}
+
+
+void Room::setRight(Room* newVal) {
+	this->_right = newVal;
+	newVal->_left = this;
+}
+
+
+const Room::ItemMap& Room::items() const {
+	return this->_items;
+}
+
+
+void Room::setItems(const ItemMap& newVal) {
+	this->_items = newVal;
+}
+
+
+// Constructor, deconstructor, and operators
+Room::Room(void) :
+_up(nullptr),
+_down(nullptr),
+_left(nullptr),
+_right(nullptr) {
+}
+
+
+Room::Room(const Room& other) {
+	this->_up = other._up;
+	this->_down = other._down;
+	this->_left = other._left;
+	this->_right = other._right;
+	this->_items = other._items;
+}
+
+
+Room::~Room(void) {
+}
+
+
+Room& Room::operator=(const Room& rhs) {
+	this->_up = rhs._up;
+	this->_down = rhs._down;
+	this->_left = rhs._left;
+	this->_right = rhs._right;
+	this->_items = rhs._items;
+
+	return *this;
+}
+
+
+// Methods
+void Room::addItem(const Item& toAdd) {
+	Item* newItem = new Item;
+	*newItem = toAdd;
+
+	this->_items.insert(ItemMapType(newItem->name(), newItem));
+}
+
+
+Item& Room::removeItem(const char* itemName) {
+	Item itemCopy;
+	ItemMapIter pos = this->_items.find(itemName);
+
+	// If the specified item exists in this Room, then remove and return it
+	if (pos != this->_items.end()) {
+		itemCopy = *(pos->second);
+		this->_items.erase(pos);
+	}
+	else {
+		throw (std::out_of_range("I don't see the \"" + std::string(itemName) + "\" in this room."));
+	}
+
+	return std::move(itemCopy);
+}
+
--- TextAdventure/Room.hpp
+++ TextAdventure/Room.hpp
@@ -1,13 +1,49 @@
 #ifndef _ROOM_HPP
 #define _ROOM_HPP
 
+#include <map>
 #include "GameObject.hpp"
+#include "Item.hpp"
+#include "utils.hpp"
 
 class Room :
 public GameObject {
-public:
+typedef std::multimap<const char*, Item*, utils::strcmp> ItemMap;
+typedef ItemMap::value_type ItemMapType;
+typedef ItemMap::iterator ItemMapIter;
+
+
+private:	// Properties
+	Room* _up;
+	Room* _down;
+	Room* _left;
+	Room* _right;
+	ItemMap _items;
+
+
+public:		// Getters and setters
+	Room* up() const;
+	void setUp(Room* newVal);
+	Room* down() const;
+	void setDown(Room* newVal);
+	Room* left() const;
+	void setLeft(Room* newVal);
+	Room* right() const;
+	void setRight(Room* newVal);
+	const ItemMap& items() const;
+	void setItems(const ItemMap& newVal);
+
+
+public:		// Constructor, deconstructor, and operators
 	Room(void);
+	Room(const Room& other);
 	virtual ~Room(void);
+	Room& operator=(const Room& rhs);
+
+
+public:		// Methods
+	void addItem(const Item& toAdd);
+	Item& removeItem(const char* itemName);
 };
 
 #endif
